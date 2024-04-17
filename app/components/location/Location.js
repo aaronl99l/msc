39f1dea58b762dbfc859fc8f37d91e0738b694ff
Aaron Leon <62915096+aaronl99l@users.noreply.github.com>
2024-04-17 09:14:12 -0400

    > Logos on nav bar added > made some tailwind changes > simplifications to placement, moving away from using padding. > added a title screen module for mobile devices

--- app/components/location/Location.js
+++ app/components/location/Location.js
@@ -14,8 +14,8 @@ function Location(){
     return(
         <div>
             <div className="md:flex">
-                <div className="md:shrink-0">
-                    <p className= "my-[5%] px-[15%] pt-[0%]" style={{
+                <div className="md:shrink-0 justify-center">
+                    <p className= "flex w-[100%] my-[5%] p-[2%]" style={{
                         fontSize: "calc(15px + 1vw)",
                         color: "white",
                         filter: "drop-shadow(1px 1px 0px black)",
@@ -25,23 +25,25 @@ function Location(){
                         filter: "drop-shadow(3px 3px 5px black)"
                     }}>Come check out our store in person or online!</p>
 
-                    <button className="mx-[50%]" onClick={sayHello} style={{
-                        backgroundColor: "red",
-                        borderRadius: "8px",
-                        height: "10vh",
-                        width: "20vw",
-                        color: "white",
-                        fontWeight: "bold",
-                        filter: "drop-shadow(2px 2px 3px black)"
-                    }}>ONLINE</button>
+                    <div className="flex w-[100%] justify-center">
+                        <button className="mx-auto" onClick={sayHello} style={{
+                            backgroundColor: "red",
+                            borderRadius: "8px",
+                            height: "8vh",
+                            width: "25vh",
+                            color: "white",
+                            fontWeight: "bold",
+                            filter: "drop-shadow(2px 2px 3px black)"
+                        }}>ONLINE</button>
+                    </div>
                 </div>
-                <div className="flex w-[50%]">
+                <div className="flex w-[100%] lg:justify-center">
                    
-                <iframe className="mx-[5%] my-[5%]" width="600" height="400" loading="lazy" allowFullScreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJNY3BydRD54kRS4KB9Dw6ik8&key=AIzaSyCWNzRf3Q9H_mB3LqhskSeAhodJs1Odgqw" style={{
-                    border: "none",
-                    borderRadius: "8px",
-                    
-                }}></iframe> 
+                    <iframe className="my-[5%]" width="600" height="400" loading="lazy" allowFullScreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJNY3BydRD54kRS4KB9Dw6ik8&key=AIzaSyCWNzRf3Q9H_mB3LqhskSeAhodJs1Odgqw" style={{
+                        border: "none",
+                        borderRadius: "8px",
+                        
+                    }}></iframe> 
                 </div>
             </div>
         </div>
--- app/components/nav/Nav.js
+++ app/components/nav/Nav.js
@@ -14,46 +14,104 @@ function Nav(){
     }
     return(
         <div>
-            <div className="header flex w-[100%] py-[30px]">
-                <nav className="flex w-[100%] fixed top-0 h-15" style={{
-                        height: "8vh",
+            <div className="header flex w-[100%] py-[30px] justify-center">
+                <nav className="w-[100%] fixed top-0" style={{
+                        
                         backgroundColor: "black",
                         zIndex: "99"
                     }}>
+                        <div className='flex w-[100%] md:w-[50%] md:mx-[25%] my-[1%] justify-center' style={{
+                            borderBottomStyle: "single",
+                            borderTopStyle: "none",
+                            borderLeftStyle: "none",
+                            borderRightStyle: "none",
+                            borderWidth: "3px",
+                            marginBottom: "0px"
+                        
+                        }}>
+                            <div className="flex w-[100%] gap-[20px] justify-between">
 
-                        <div className="flex w-[85%] gap-[10px]">
-                            <div>
-                                <Image className="py-[5px] px-[5px]" src="/assets/logo.jpg" width={50} height={50} alt="Business Logo" style={{
-                                    maxHeight: "50px",
-                                    maxWidth: "50px"
-                                }}/>
-                            </div>
+                                <div className=" justify-start">
+                                    <Image className="my-auto" src="/assets/logo.jpg" width={50} height={50} alt="Business Logo" style={{
+                                        maxHeight: "50px",
+                                        maxWidth: "50px"
+                                    }}/>
+                                </div>
 
-                            <div className="py-[0px] px-[5px]">
-                                <p className={bangers.className} style=
-                                {{
-                                    color: "red", 
-                                    fontSize: "calc(8px + 2vw)",
-                                    filter: "drop-shadow(1px 1px 0px white)"
-                                }}>Main Street Comics Toys & More</p>
+                                <div className="justify-center my-auto hidden lg:block">
+                                    <p className={bangers.className} style=
+                                    {{
+                                        color: "red", 
+                                        fontSize: "calc(8px + 2vw)",
+                                        filter: "drop-shadow(1px 1px 0px white)"
+                                    }}>Main Street Comics Toys & More</p>
+                                </div>
+                            
+                            
+                            
+                                <div className=" justify-end">
+                                    <button onClick={btnShout}>
+                                    <Image src="/assets/hamburg_red.png" width={50} height={50} alt="Menu" style={{
+                                        maxHeight: "50px",
+                                        maxWidth: "50px",
+                                    }}/>
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                        
-                        <div className="flex w-[15%] justify-end">
-                            <div className=" px-[10px]">
-                                <button onClick={btnShout}>
-                                <Image src="/assets/hamburg_red.png" width={50} height={50} alt="Menu" style={{
-                                    maxHeight: "35px",
-                                    maxWidth: "35px",
-                                    margin: "0 auto",
-                                    marginTop: "0.5vw"
+
+                        <div className="w-[100%] hidden md:block">
+                            <div className='flex gap-[2.5%] justify-center' style={{
+                            
+                            }}>
+                                <Image src="/assets/logos/mythleg.png" height={50} width={50} alt="temp" style={{
+                                    objectFit: "contain",
+                                    width: "3vw",
+                                    height: "auto"
                                 }}/>
-                                </button>
+                                <Image src="/assets/logos/gws.png" height={50} width={50} alt="temp" style={{
+                                    objectFit: "contain",
+                                    width: "3vw",
+                                    height: "auto"
+                                }}/>
+                                <Image src="/assets/logos/marvel.png" height={50} width={50} alt="temp" style={{
+                                    objectFit: "contain",
+                                    width: "3vw",
+                                    height: "auto"
+                                }}/>
+                                <Image src="/assets/logos/dc_comics.png" height={60} width={60} alt="temp" style={{
+                                    objectFit: "contain",
+                                    width: "3vw",
+                                    height: "auto"
+                                }}/>
+                                <Image src="/assets/logos/pkmn.png" height={50} width={50} alt="temp" style={{
+                                    objectFit: "contain",
+                                    width: "3vw",
+                                    height: "auto"
+                                }}/>
+                                <Image src="/assets/logos/mtg.png" height={50} width={50} alt="temp" style={{
+                                    objectFit: "contain",
+                                    width: "3vw",
+                                    height: "auto"
+                                }}/>
+                                <Image src="/assets/logos/starwars.png" height={50} width={50} alt="temp" style={{
+                                    objectFit: "contain",
+                                    width: "3vw",
+                                    height: "auto"
+                                }}/>
+                                <Image src="/assets/logos/dbz.png" height={50} width={50} alt="temp" style={{
+                                    objectFit: "contain",
+                                    width: "3vw",
+                                    height: "auto"
+                                }}/>
+                                <Image src="/assets/logos/gojira.png" height={50} width={50} alt="temp" style={{
+                                    objectFit: "contain",
+                                    width: "3vw",
+                                    height: "auto"
+                                }}/>
                             </div>
                         </div>
-                        
                 </nav>
-                <div className="w-[100%]"></div>
             </div>
         </div>
 
