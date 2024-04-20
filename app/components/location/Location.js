74adf4784d6038e02a086d35b154fddea402fbf9
Aaron Leon <62915096+aaronl99l@users.noreply.github.com>
2024-04-20 11:21:52 -0400

    Updated Location.js
    Added some stylistic changes to the location.js component. Aiming for a "comic-book" feel. Almost there, just need to re-do store hours with accurate information and add backdrop images with an opacity filter to allow for an easy reading experience.

--- app/components/location/Location.js
+++ app/components/location/Location.js
@@ -11,81 +11,97 @@ function Location(){
     function sayHello(){
         console.log("");
     }
+    /*
+                        <div className="">
+                            <img src="/assets/bg_imgs/horseman-war.jpg" style={{
+                                opacity: "0.6",
+                                zIndex: "1",
+                                width: "100%",
+                                height: "auto"
+                            }}></img>
+                        </div>
+    */
     return(
         <div className="h-[100vh]">
-            <div className="md:flex">
-                <div className="md:shrink-0 justify-center">
-                    <p className= "flex w-[100%] my-[5%] p-[2%]" style={{
-                        fontSize: "calc(15px + 1vw)",
-                        color: "white",
-                        filter: "drop-shadow(1px 1px 0px black)",
-                        fontWeight: "bold",
-                        textAlign: "center",
-                        backgroundColor: "black",
-                        filter: "drop-shadow(3px 3px 5px black)"
-                    }}>Come check out our store in person or online!</p>
+            
+                <div className="md:flex w-[100%] md:gap-4 md:h-[42vh] md:mt-[4%] justify-center">
+                    <div className="md:flex md:w-[38%] md:shrink-0 justify-center md:border-solid md:border-black md:border-[7px] 
+                                    bg-[url('/assets/bg_imgs/horseman-war.jpg')]
+                                    bg-opacity-0">
+                        <div className="w-[100%] h-[18vh] md:my-[15%] md:mx-[5%]">
+                            <div className="">
+                                <p className="flex w-[100%]" style={{
+                                    fontSize: "calc(15px + 1vw)",
+                                    color: "white",
+                                    filter: "drop-shadow(1px 1px 3px black)",
+                                    fontWeight: "bold",
+                                    textAlign: "center",
+                                    
+                                }}>Come check out our store in person or online!</p>
+                            </div>
+                            <div className="flex w-[100%] justify-center">
+                                <button className="mx-auto" onClick={sayHello} style={{
+                                    backgroundColor: "red",
+                                    borderRadius: "8px",
+                                    height: "8vh",
+                                    width: "25vh",
+                                    color: "white",
+                                    fontWeight: "bold",
+                                    filter: "drop-shadow(2px 2px 3px black)"
+                                }}>ONLINE</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="md:flex md:w-[58%] w-[100%] my-[4%] md:my-[0%]" style={{
+                    }}>
+                    
+                        <iframe className="w-[100%] md:h-[100%] md:border-solid md:border-black md:border-[7px]" width="400" height="400" loading="lazy" allowFullScreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJNY3BydRD54kRS4KB9Dw6ik8&key=AIzaSyCWNzRf3Q9H_mB3LqhskSeAhodJs1Odgqw" style={{
 
-                    <div className="flex w-[100%] justify-center">
-                        <button className="mx-auto" onClick={sayHello} style={{
-                            backgroundColor: "red",
-                            borderRadius: "8px",
-                            height: "8vh",
-                            width: "25vh",
-                            color: "white",
-                            fontWeight: "bold",
-                            filter: "drop-shadow(2px 2px 3px black)"
-                        }}>ONLINE</button>
-                    </div>
+                        }}></iframe> 
+                    </div>  
                 </div>
-                <div className="flex w-[100%] lg:justify-center">
-                   
-                    <iframe className="my-[5%]" width="600" height="400" loading="lazy" allowFullScreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJNY3BydRD54kRS4KB9Dw6ik8&key=AIzaSyCWNzRf3Q9H_mB3LqhskSeAhodJs1Odgqw" style={{
-                        border: "none",
-                        borderRadius: "8px",
-                        
-                    }}></iframe> 
-                </div>   
-            </div>
-            <div className="flex w-[100%] justify-center">
-                    <div className="">
-                        <p className="" style={{
-                            color: "black",
-                            fontWeight: "bold",
-                            fontSize: "calc(12px + 0.5vh)"
-                        }}>Monday   -----   -----   9:00am - 6:00pm</p>
-                        <p className="" style={{
-                            color: "black",
-                            fontWeight: "bold",
-                            fontSize: "calc(12px + 0.5vh)"
-                        }}>Tuesday  -----   -----   9:00am - 6pm</p>
-                        <p className="" style={{
-                            color: "black",
-                            fontWeight: "bold",
-                            fontSize: "calc(12px + 0.5vh)"
-                        }}>Wednesday  -----   -----   9:00am - 6pm</p>
-                        <p className="" style={{
-                            color: "black",
-                            fontWeight: "bold",
-                            fontSize: "calc(12px + 0.5vh)"
-                        }}>Thursday  -----   -----   9:00am - 6pm</p>
-                        <p className="" style={{
-                            color: "black",
-                            fontWeight: "bold",
-                            fontSize: "calc(12px + 0.5vh)"
-                        }}>Friday  -----   -----   9:00am - 6pm</p>
-                        <p className="" style={{
-                            color: "black",
-                            fontWeight: "bold",
-                            fontSize: "calc(12px + 0.5vh)"
-                        }}>Saturday  -----   -----   9:00am - 6pm</p>
-                        <p className="" style={{
-                            color: "black",
-                            fontWeight: "bold",
-                            fontSize: "calc(12px + 0.5vh)"
-                        }}>Sunday  -----   -----   9:00am - 6pm</p>
-                        
+                    <div className="flex w-[100%] justify-center">
+                        <div className="flex w-[97%] justify-center md:mt-[0.75%] md:border-solid md:border-black md:border-[7px]">
+                            <div className="">
+                                <p className="" style={{
+                                    color: "black",
+                                    fontWeight: "bold",
+                                    fontSize: "calc(12px + 0.5vh)"
+                                }}>Monday   -----   -----   9:00am - 6:00pm</p>
+                                <p className="" style={{
+                                    color: "black",
+                                    fontWeight: "bold",
+                                    fontSize: "calc(12px + 0.5vh)"
+                                }}>Tuesday  -----   -----   9:00am - 6pm</p>
+                                <p className="" style={{
+                                    color: "black",
+                                    fontWeight: "bold",
+                                    fontSize: "calc(12px + 0.5vh)"
+                                }}>Wednesday  -----   -----   9:00am - 6pm</p>
+                                <p className="" style={{
+                                    color: "black",
+                                    fontWeight: "bold",
+                                    fontSize: "calc(12px + 0.5vh)"
+                                }}>Thursday  -----   -----   9:00am - 6pm</p>
+                                <p className="" style={{
+                                    color: "black",
+                                    fontWeight: "bold",
+                                    fontSize: "calc(12px + 0.5vh)"
+                                }}>Friday  -----   -----   9:00am - 6pm</p>
+                                <p className="" style={{
+                                    color: "black",
+                                    fontWeight: "bold",
+                                    fontSize: "calc(12px + 0.5vh)"
+                                }}>Saturday  -----   -----   9:00am - 6pm</p>
+                                <p className="" style={{
+                                    color: "black",
+                                    fontWeight: "bold",
+                                    fontSize: "calc(12px + 0.5vh)"
+                                }}>Sunday  -----   -----   9:00am - 6pm</p>
+                                
+                            </div>
+                        </div>
                     </div>
-            </div>
         </div>
 )}
 
