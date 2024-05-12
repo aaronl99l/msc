cf98ae924b96f6157a2f6729ce85fb94f9984f79
Aaron Leon <62915096+aaronl99l@users.noreply.github.com>
2024-05-12 08:45:13 -0400

    Form Handling

--- app/components/contact/Contact.js
+++ app/components/contact/Contact.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 
 export default function Contact(){
+
     //States for all form fields
     const [fullname, setFullname] = useState("");
     const [email, setEmail] = useState("");
@@ -28,17 +29,17 @@ export default function Contact(){
 
         if (isValidForm) {
             setButtonText("Sending");
-            const res = await fetch("/api/sendgrid", {
+            const res = await fetch("/app/api/sendgrid.js", {
                 body: JSON.stringify({
                     email: email,
                     fullname: fullname,
                     subject: subject,
-                    message: message,
+                    message: message
                 }),
                 headers: {
-                    "Content-Type": "application/json", 
+                    "Content-Type": "application/json" 
                 },
-                method: "POST",
+                method: "POST"
             });
 
             const { error } = await res.json();
@@ -47,11 +48,23 @@ export default function Contact(){
                 setShowSuccessMessage(false);
                 setShowFailureMessage(true);
                 setButtonText("Send");
+
+                //resetting fields
+                setFullname("");
+                setEmail("");
+                setMessage("");
+                setSubject("");
                 return;
             }
             setShowSuccessMessage(true);
             setShowFailureMessage(false);
             setButtonText("Send");
+
+            //rest form fields
+            setFullname("");
+            setEmail("");
+            setMessage("");
+            setSubject("");
         }
         console.log(fullname, email, subject, message);
     };
@@ -80,12 +93,12 @@ export default function Contact(){
         setErrors({ ...tempErrors});
         console.log("errors", errors);
         return isValid;
-    }
+    };
 
     return(
         <div style={{
             backgroundColor: "black"
-        }}>
+        }}>   
             
             <form onSubmit={ handleSubmit }
             className="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-black dark:bg-black-500">
@@ -106,7 +119,11 @@ export default function Contact(){
                     setFullname(e.target.value);
                     }}
                     name="fullname"
-                    className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-red-500 font-light text-gray-500"/>
+                    className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-red-500 font-light text-gray-500"
+                />
+                {errors?.fullname && (
+                    <p className="text-red-500">Fullname cannot be empty.</p>
+                )}
                 
 
                 <label
@@ -121,7 +138,11 @@ export default function Contact(){
                     onChange={(e) => {
                     setEmail(e.target.value);
                     }}
-                    className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-red-500 font-light text-gray-500"/>
+                    className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-red-500 font-light text-gray-500"
+                />
+                {errors?.email && (
+                    <p className="text-red-500">Email cannot be empty.</p>
+                )}
                 
 
                 <label
@@ -136,7 +157,12 @@ export default function Contact(){
                     onChange={(e) => {
                     setSubject(e.target.value);
                     }}
-                    className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-red-500 font-light text-gray-500"/>
+                    className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-red-500 font-light text-gray-500"
+                />
+                {errors?.subject && (
+                    <p className="text-red-500">Subject cannot be empty.</p>
+                )}
+
                 
                 <label
                     htmlFor="message"
@@ -149,13 +175,17 @@ export default function Contact(){
                     onChange={(e) => {
                     setMessage(e.target.value);
                     }}
-                    className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-red-500 font-light text-gray-500"></textarea>
+                    className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-red-500 font-light text-gray-500">
+                </textarea>
+                {errors?.message && (
+            <       p className="text-red-500">Message body cannot be empty.</p>
+                )}
                 
                 <div className="flex flex-row items-center justify-start">
                     <button
                     type="submit"
                     className="px-10 mt-8 py-2 bg-[red] text-gray-50 font-light rounded-md text-lg flex flex-row items-center">
-                    Submit
+                    {buttonText}
                     <svg
                         width="24"
                         height="24"
@@ -168,6 +198,19 @@ export default function Contact(){
                         fill="white"/>
                     </svg>
                     </button>
+                </div>
+                
+                <div className="text-left">
+                    {showSuccessMessage && (
+                    <p className="text-green-500 font-semibold text-sm my-2">
+                        Thankyou! Your Message has been delivered.
+                    </p>
+                    )}
+                    {showFailureMessage && (
+                    <p className="text-red-500">
+                        Oops! Something went wrong, please try again.
+                    </p>
+                    )}
                 </div>
             </form>
         </div>
